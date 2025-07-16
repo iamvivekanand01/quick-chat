a40055f2f6f3f598d4bd2187eb27c105485a4ac4
Vivekanand <iam.vivekanand01@gmail.com>
2025-07-16 22:57:21 +0530

    fixed WebSocket config

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -4,6 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
+console.log("Loaded Backend URL from env:", backendUrl);
 
 axios.defaults.baseURL = backendUrl;
 
